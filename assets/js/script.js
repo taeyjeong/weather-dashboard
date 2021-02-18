d3647ae54615d4de269e45bf6a918264d5fe76c4
TaeJeong <i@taeyjeong.com>
2021-02-18 12:03:07 -0800

    able to fetch data for temperature, humidity, and wind speed for the city in the present time

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#submit').on('click', function() {
+    fetch( // FETCHING THE DATA FOR TODAY
+      'https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=4f4fc4b2d81e48700fb99a35e316ea42'
+    )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      $('#city').html(data['name']) // INSERTING THE FETCHED CITY DATA
+      $('#temp').html(data['main']['temp']) // INSERTING THE FETCHED TEMP DATA
+      $('#humid').html(data['main']['humidity']) // INSERTING THE FETCHED HUMIDITY DATA
+      $('#wind').html(data['wind']['speed']) // INSERTING THE FETCHED WIND SPEED DATA
+      console.log(data)
+    })
+  })
+})
